4482df5fb7a2b9c2658b586c32a82687ce18ab30
Gabriela Laslau <lg.gabriela@yahoo.com>
2021-11-10 19:33:14 +0200

    first commit

---
+++ src/pages/HomePage/Header.js
@@ -0,0 +1,29 @@
+import { Text , View } from 'react-native';
+import styles from './HomePage.styles';
+
+const Header = () => {
+    return (
+        <View style={styles.view}>
+            <Text style={styles.textBigger}>
+               Hi,folks!
+            </Text>
+            <Text style={styles.text}>
+               Welcome on board!
+            </Text>
+            <Text style={styles.textSmaller}>
+    
+            </Text> 
+            <Text style={styles.textSmaller}>
+             Make some memories with this new photo app!
+            </Text> 
+            <Text style={styles.textSmaller}>
+              Bring your friends    ..
+            </Text>
+            <Text style={styles.textSmaller}>
+               and your smile!
+            </Text>
+        </View>
+    )
+}
+
+export default Header;
